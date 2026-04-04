8c1219df6441619aa7ae11a28520474572e5a129
stevecopal <copalsteve@gmail.com>
2026-04-04 19:32:05 +0100

    bd modif

--- eaupourtous/settings.py
+++ eaupourtous/settings.py
@@ -109,12 +109,24 @@ WSGI_APPLICATION = 'eaupourtous.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('POSTGRES_DB'),
+            'USER': config('POSTGRES_USER'),
+            'PASSWORD': config('POSTGRES_PASSWORD'),
+            'HOST': config('POSTGRES_HOST', default='localhost'),
+            'PORT': config('POSTGRES_PORT', default=5432, cast=int),
+        }
+    }
 
 
 # Password validation
